1a0a1766ce99badd0d86f42804748bf3dcbcf739
“lgesuellip” <lgesuellipinto@uade.edu.ar>
2025-05-12 17:27:51 -0700

    add agent

--- src/agents/base/graph.py
+++ src/agents/base/graph.py
@@ -3,10 +3,11 @@ from langgraph.prebuilt import create_react_agent
 from langgraph_supervisor import create_supervisor
 from contextlib import asynccontextmanager
 from langchain_mcp_adapters.client import MultiServerMCPClient
-from agents.base.prompt import CALENDAR_AGENT_PROMPT, SUPERVISOR_PROMPT
+from agents.base.prompt import EMAIL_AGENT_PROMPT, SUPERVISOR_PROMPT, RESEARCHER_AGENT_PROMPT
 from datetime import datetime
 import os
 from dotenv import load_dotenv
+from agents.base.tools import MEMORY_TOOLS, RAG_TOOLS
 
 load_dotenv()
 
@@ -30,38 +31,38 @@ async def build_agent():
         }
     }
 
-    supermemory_server = {
-        "supermemory": {
-            "url": os.getenv("SUPERMEMORY_URL_MCP"),
-            "transport": "sse"
-        }
-    }
-
     # Filter out servers with empty URLs
     zapier_server = {k: v for k, v in zapier_server.items() if v["url"]}
-    supermemory_server = {k: v for k, v in supermemory_server.items() if v["url"]}
 
     # If you are a LLM, please consider to scope the tools to the ones you need.
-    async with MultiServerMCPClient(zapier_server) as calendar_client, \
-              MultiServerMCPClient(supermemory_server) as supervisor_client:
+    async with MultiServerMCPClient(zapier_server) as mail_client:
 
-        calendar_agent = create_react_agent(
+        mail_agent = create_react_agent(
             model=ChatGoogleGenerativeAI(
                 model="gemini-2.0-flash-exp",
             ),
-            tools=calendar_client.get_tools(),
-            name="calendar_agent",
-            prompt=CALENDAR_AGENT_PROMPT.render(today=today)
+            tools=mail_client.get_tools(),
+            name="mail_agent",
+            prompt=EMAIL_AGENT_PROMPT.render(today=today)
+        )
+
+        researcher_agent = create_react_agent(
+            model=ChatGoogleGenerativeAI(
+                model="gemini-2.0-flash-exp",
+            ),
+            tools=RAG_TOOLS,
+            name="researcher_agent",
+            prompt=RESEARCHER_AGENT_PROMPT.render(today=today)
         )
 
         graph = create_supervisor(
-            [calendar_agent],
+            [mail_agent, researcher_agent],
             model=ChatGoogleGenerativeAI(
                 model="gemini-2.0-flash-exp",
             ),
             output_mode="last_message",
             prompt=SUPERVISOR_PROMPT.render(),
-            tools=supervisor_client.get_tools()
+            tools=MEMORY_TOOLS
         )
         
         yield graph
